1cb04d74e29ae91bd77e4c627ef43f7744a004a0
Alexander Grenkov <a.grenkov@gmail.com>
2024-09-02 17:51:52 +0500

    add: register, login and logout functionality

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">Cats in a house is {{ count }}</button>
+    <button type="button" @click="count+=2">Cats in a house is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import './style.css' // Using the default Vite CSS. Replace with your own global styles.
+import router from './router'
 import App from './App.vue'
+import { useAuthStore } from './store/auth'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+const authStore = useAuthStore()
+authStore.setCsrfToken()
+
+app.mount('#app')
